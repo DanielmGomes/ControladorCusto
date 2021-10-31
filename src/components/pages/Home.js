23f18d12e76f4aad30602e0c2d352f5992aa9718
DanielmGomes <danielmoreiragomes.dmg@gmail.com>
2021-10-31 11:14:06 -0300

    home

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,9 +1,18 @@
+import styles from './Home.module.css'
+import savings from '../../img/money.svg'
+import LinkButton from '../layout/LinkButton'
+
+
 function Home() {
 	return (
-		<div>
-			<h1>Home</h1>
-			<p>conteudo home</p>
-		</div>
+
+	<section className={styles.home_container}>
+		<h1>Bem-vindo ao controlador de  <span>custos</span></h1>
+		<p>Gerencie seus projetos pessoais e os custos para realiza-los</p>
+		<LinkButton to='/newProject' text='criar projeto'/>
+		<img src={savings} alt="controlador de custos" />
+	</section>
+
 	)
 }
 
